4479f7fd4d2d42f6eeffde81a307cd52011099a8
saiprasad-hydraboiny <prasadhydraboiny9122@gmail.com>
2024-02-22 18:37:12 +0530

    backend intial commit

---
+++ src/main/java/com/example/movies/controllers/MovieController.java
@@ -0,0 +1,33 @@
+package com.example.movies.controllers;
+
+
+import com.example.movies.models.Movie;
+import com.example.movies.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
+
+    }
+}
---
+++ src/main/java/com/example/movies/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.example.movies.repositories;
+
+import com.example.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
